806334fe1702a86ca200654bf7003577cfe45d59
Qian Cai <caiqianlucy@gmail.com>
2020-03-30 09:13:06 -0700

    FindNode

---
+++ LeetCodeAnswers/src/LeetCode1379.java
@@ -0,0 +1,33 @@
+/*author@Qian Cai
+ * Title@Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+ * Given two binary trees original and cloned and given a reference to a node target in the original tree.
+
+The cloned tree is a copy of the original tree.
+
+Return a reference to the same node in the cloned tree.
+
+Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
+
+Follow up: Solve the problem if repeated values on the tree are allowed
+ * 
+ */
+
+public class LeetCode1379 {
+	/**
+	 * Definition for a binary tree node.
+	 */
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	  public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+	        if (original == target || original == null) return cloned;
+	        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+	        if (left != null) return left;
+	        TreeNode right = getTargetCopy(original.right, cloned.right, target);
+	        return right;
+	        
+	    }
+}

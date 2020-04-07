a9613f71dadd5d1d9cb20917d106cf84c95023b9
Qian Cai <caiqianlucy@gmail.com>
2020-04-06 18:21:40 -0700

    logn

--- LeetCodeAnswers/src/LeetCode1399.java
+++ LeetCodeAnswers/src/LeetCode1399.java
@@ -3,11 +3,12 @@
  * Given an integer n. Each number from 1 to n is grouped according to the sum of its digits. 
 
 Return how many groups have the largest size.
- * Time, Space: O(n)
+ * 1. Time, Space: O(n)
+ * 2. Time, Space: o(logn)
  */
 import java.util.*;
 public class LeetCode1399 {
-	public int countLargestGroup(int n) {
+	public static int countLargestGroup(int n) {
         Map<Integer, Integer> map = new HashMap();
         int mCnt = 0, res = 0;
         for (int i = 1; i <= n; i++){
@@ -22,12 +23,60 @@ public class LeetCode1399 {
         }
         return res;
     }
-    public int helper(int n){
+    public static int helper(int n){
         int res = 0;
         while (n != 0){
             res += n%10;
             n/=10;
         }
         return res;
+    }
+    public static int countLargestGroup2(int n) {
+    	String s = binary(n);
+    	int m = s.length();
+    	int[] count = new int[m+1];
+    	int prev = 0;
+    	for (int i = 0; i < m; i++) {
+    		if (s.charAt(i) == 1) {
+    			update(count, m-i-1, prev++);//counts from 1 to 10000
+    		}
+    		
+    	}
+    	int max = 0;
+    	for (int c: count) {
+    		if (c > max) max = c;
+    	}
+    	int res = 0;
+    	for (int c: count) {
+    		if (c == max) res++;
+    	}
+    	return res;
+    }
+    public static void update(int[] count, int n, int prev) {
+    	for (int i = 0; i <= n; i++) {
+    		count[prev+i] += combination(n, i);	
+    	}
+    	count[1]++;
+    }
+    public static int combination(int n, int i) {
+    	int one = 1, two = 1;
+    	while (i > 0) {
+    		one *= n--;
+    		two *= i--;
+    	}
+    	return one/two;
+    }
+    
+    public static String binary(int n){
+        StringBuilder sb = new StringBuilder();
+        while (n != 0){
+            sb.insert(0, n%2);
+            n/=2;
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+    	System.out.println(countLargestGroup2(13));
+    	
     }
 }

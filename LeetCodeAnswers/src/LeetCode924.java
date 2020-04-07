bd572d77303e811bb7d13d8d9f5e6ad56887f997
Qian Cai <caiqianlucy@gmail.com>
2020-04-07 07:49:34 -0700

    Minimize Malware Spread

---
+++ LeetCodeAnswers/src/LeetCode924.java
@@ -0,0 +1,74 @@
+/*author@ Qian Cai
+ * Title@ Minimize Malware Spread
+ * In a network of nodes, each node i is directly connected to another node j if and only if graph[i][j] = 1.
+
+Some nodes initial are initially infected by malware.  Whenever two nodes are directly connected and at least one of those two nodes is infected by malware, both nodes will be infected by malware.  This spread of malware will continue until no more nodes can be infected in this manner.
+
+Suppose M(initial) is the final number of nodes infected with malware in the entire network, after the spread of malware stops.
+
+We will remove one node from the initial list.  Return the node that if removed, would minimize M(initial).  If multiple nodes could be removed to minimize M(initial), return such a node with the smallest index.
+
+Note that if a node was removed from the initial list of infected nodes, it may still be infected later as a result of the malware spread.
+ * Time: O(n*n) Space: O(n), Union Find
+ */
+import java.util.Arrays;
+public class LeetCode924 {
+	 public int minMalwareSpread(int[][] graph, int[] initial) {
+	        int n = graph.length;
+	        DSU dsu = new DSU(n);
+	        for (int i = 0; i < n; i++){
+	            for (int j = 0; j < n; j++){
+	                if (graph[i][j] == 1) dsu.union(i, j);
+	            }
+	        }
+	        int ans = -1,  ansSize = -1;
+	        int[] count = new int[n]; //find the node in the initial that is not connected with other nodes in initial
+	        for (int node: initial){
+	            count[dsu.parent[node]]++;
+	        }
+	        for (int node: initial){
+	            int root = dsu.find(node);
+	            if (count[root] == 1){
+	                if (dsu.size[root] > ansSize || (dsu.size[root] == ansSize && node < ans)){
+	                    ans = node;
+	                    ansSize = dsu.size[root];
+	                }
+	            }
+	        }
+	        //if there is no unique node, return the node with smallest value
+	        if (ans == -1){
+	            ans = Integer.MAX_VALUE;
+	            for (int node: initial) ans = Math.min(ans, node);
+	        }
+	        return ans;
+	    }
+	    public class DSU{
+	        int[] parent;
+	        int[] size;  //use size instead of rank because for getting the node to remove need size information
+	        public DSU(int n){
+	            parent = new int[n];
+	            size = new int[n];
+	            Arrays.fill(size, 1);
+	            for (int i = 0; i < n; i++) parent[i] = i;
+	        }
+	        public int find(int x){
+	            while (x != parent[x]){
+	                parent[x] = parent[parent[x]]; //path compression
+	                x = parent[x];
+	            }
+	            return x;
+	        }
+	        public void union(int x, int y){
+	            int xP = find(x);
+	            int yP = find(y);
+	            if (xP == yP) return;
+	            if (size[xP] < size[yP]){
+	                parent[xP]=yP;
+	                size[yP] += size[xP]; 
+	            }  else {
+	                parent[yP] = xP;
+	                size[xP] += size[yP];
+	            }
+	        }  
+	    }
+}

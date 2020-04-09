f9f748e9403a4e8ddd6cfec5764a39e83d878717
Qian Cai <caiqianlucy@gmail.com>
2020-04-09 10:30:06 -0700

    Lable Partition Details

--- LeetCodeAnswers/src/LeetCode763.java
+++ LeetCodeAnswers/src/LeetCode763.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/* author@ Qian Cai
+ * Title@ Partition Lables
+ * A string S of lowercase letters is given. We want to partition this string 
+ * into as many parts as possible so that each letter appears in at most one 
+ * part, and return a list of integers representing the size of these parts.
+ * 
+ * Solution
+ * We need a lastIdxMap array to keep the last occurrence of each char
+ * and a prev index to anchor the start of each partition 
+ * cur keeps the idx of the last idx of all the char iterate through
+ * when i == cur, that is the end of one partition, update res and set prev to cur
+ */
 //Time: O(n) Space: O(1)
 public class LeetCode763 {
 	public List<Integer> partitionLabels(String S) {

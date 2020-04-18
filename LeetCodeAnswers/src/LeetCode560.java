55fe01ec9f4c7a8135db3516aa0d3a38bdb40f27
Qian Cai <caiqianlucy@gmail.com>
2020-04-17 22:02:05 -0700

    Subarray Sum Equals K

---
+++ LeetCodeAnswers/src/LeetCode560.java
@@ -0,0 +1,22 @@
+/* author@ Qian Cai
+ * Title@ Subarray Sum Equals K
+ * Given an array of integers and an integer k, you need to find the total 
+ * number of continuous subarrays whose sum equals to k.
+ * Time Space complexity: O(n)
+ */
+import java.util.Map;
+import java.util.HashMap;
+public class LeetCode560 {
+	 public int subarraySum(int[] nums, int k) {
+	        int count = 0;
+	        int sum = 0;
+	        Map<Integer, Integer> map = new HashMap(); //put the prefix sum and count
+	        map.put(0, 1);
+	        for (int i = 0; i < nums.length; i++){
+	            sum += nums[i];
+	            if (map.containsKey(sum-k)) count += map.get(sum-k);
+	            map.put(sum, map.getOrDefault(sum, 0) + 1);
+	        }
+	        return count;
+	    }
+}

59f3e5b15d6093a188db373e602699cc7cbfa122
Qian Cai <caiqianlucy@gmail.com>
2020-04-18 22:32:12 -0700

    DisplayTable

--- LeetCodeAnswers/src/LeetCode1418.java
+++ LeetCodeAnswers/src/LeetCode1418.java
@@ -1,45 +1,58 @@
-/* author@ Qian Cai
- * Title@ Reformat the String
- * Given alphanumeric string s. (Alphanumeric string is a string consisting of
- *  lowercase English letters and digits).
+/*author@ Qian Cai
+ * Title@ Display Table of Food Orders in a Restaurant
+ * Given the array orders, which represents the orders that customers have done
+ *  in a restaurant. More specifically orders[i]=[customerNamei,tableNumberi,
+ *  foodItemi] where customerNamei is the name of the customer, tableNumberi 
+ *  is the table customer sit at, and foodItemi is the item customer orders.
+ *  
 
-You have to find a permutation of the string where no letter is followed by 
-another letter and no digit is followed by another digit. That is, no two
- adjacent characters have the same type.
-
-Return the reformatted string or return an empty string if it is impossible
- to reformat the string.
+Return the restaurant's “display table”. The “display table” is a table whose 
+row entries denote how many of each food item each table ordered. The first 
+column is the table number and the remaining columns correspond to each food 
+item in alphabetical order. The first row should be a header whose first 
+column is “Table”, followed by the names of the food items. Note that the 
+customer names are not part of the table. Additionally, the rows should be 
+sorted in numerically increasing order.
  * 
  */
+import java.util.*;
 public class LeetCode1418 {
-	public String reformat(String s) {
-        StringBuilder dig = new StringBuilder();
-        StringBuilder let = new StringBuilder();
-        int i = 0;
-        while (i < s.length()){
-            if (Character.isDigit(s.charAt(i))){
-                dig.append(s.charAt(i));
-
-            } else{
-                let.append(s.charAt(i));
-            }
-            i++;
-        }
-        int len1 = dig.length(), len2 = let.length();
-        if (Math.abs(len1-len2) > 1) return "";
-        
-        StringBuilder res = new StringBuilder();
-        for (int j = 0; j < Math.min(len1, len2); j++){
-            res.append(dig.charAt(j));
-            res.append(let.charAt(j));
-        }
-        if (len1 == len2) return res.toString();
-        if (len1 > len2){
-            res.append(dig.charAt(len1-1));
-            return res.toString();
-        }
-        res.insert(0, let.charAt(len2-1));
-        return res.toString();
-        
-    }
+	 public List<List<String>> displayTable(List<List<String>> orders) {
+	        List<Integer> tableList = new ArrayList();
+	        Set<String> dishSet = new HashSet();
+	        HashMap<Integer, HashMap<String, Integer>> table = new HashMap();
+	        for (List<String> order: orders){
+	            int tableId = Integer.parseInt(order.get(1));
+	            String dish = order.get(2);
+	            dishSet.add(dish);
+	            if (!table.containsKey(tableId)){
+	                table.put(tableId, new HashMap());
+	                tableList.add(tableId);
+	            } 
+	            HashMap<String, Integer> orderMap = table.get(tableId);
+	            orderMap.put(dish, orderMap.getOrDefault(dish, 0)+1);
+	        }
+	        List<String> dishList = new ArrayList<String>(dishSet);
+	        Collections.sort(tableList);
+	        Collections.sort(dishList);
+	        dishList.add(0, "Table");
+	        List<List<String>> res = new ArrayList();
+	        res.add(dishList);
+	        for (int i = 0; i < tableList.size(); i++){
+	            List<String> row = new ArrayList();
+	            int tableId = tableList.get(i);
+	            HashMap<String, Integer> orderMap = table.get(tableId);
+	            row.add(""+tableId);
+	            for (int j = 1; j < dishList.size(); j++){
+	                String dish = dishList.get(j);
+	                if (orderMap.containsKey(dish)){
+	                    row.add(orderMap.get(dish).toString());
+	                } else {
+	                    row.add("0");
+	                }
+	            }
+	            res.add(row);
+	        }
+	        return res;
+	    }
 }

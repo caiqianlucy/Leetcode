ed36d70b7fc23cbd08d0a54daf9d4f752bc288d7
Qian Cai <caiqianlucy@gmail.com>
2020-07-06 22:50:14 -0700

    Minimum Swaps To Make Sequences Increasing

---
+++ LeetCodeAnswers/src/LeetCode801.java
@@ -0,0 +1,49 @@
+/* author@ Qian Cai
+ * Title@ Minimum Swaps To Make Sequences Increasing
+ * We have two integer sequences A and B of the same non-zero length.
+
+We are allowed to swap elements A[i] and B[i].  Note that both elements are in the same index position in their respective sequences.
+
+At the end of some number of swaps, A and B are both strictly increasing.  (A sequence is strictly increasing if and only if A[0] < A[1] < A[2] < ... < A[A.length - 1].)
+
+Given A and B, return the minimum number of swaps to make both sequences strictly increasing.  It is guaranteed that the given input always makes it possible.
+
+Example:
+Input: A = [1,3,5,4], B = [1,2,3,7]
+Output: 1
+Explanation: 
+Swap A[3] and B[3].  Then the sequences are:
+A = [1, 3, 5, 7] and B = [1, 2, 3, 4]
+which are both strictly increasing.
+Note:
+
+A, B are arrays with the same length, and that length will be in the range [1, 1000].
+A[i], B[i] are integer values in the range [0, 2000].
+ * 
+ */
+public class LeetCode801 {
+	 /* time: O(n)
+    dynamic programming 
+    every index there is two states either swap or not swap the two numbers
+    current state is only dependent on the previous state
+    */
+    public int minSwap(int[] A, int[] B) {
+        int n = A.length; 
+        int pn = 0, ps = 1; //pn: swap counts for previous number not swap, ps: swap counts for previous number swapped
+        for (int i = 1; i < n; i++){
+            int cn = Integer.MAX_VALUE; 
+            int cs = Integer.MAX_VALUE;
+            if (A[i-1] < A[i] && B[i-1] < B[i]){
+                cn = Math.min(cn, pn);
+                cs = Math.min(cs, ps+1);
+            } 
+            if (A[i-1] < B[i] && B[i-1] < A[i]){
+                cn = Math.min(ps, cn);
+                cs = Math.min(cs, pn + 1);
+            }
+            pn = cn;
+            ps = cs;
+        }
+        return Math.min(pn, ps);
+    }
+}
